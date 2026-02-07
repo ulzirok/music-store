05134b5ae1345fe8c90ef3b949797d2e66ebfd39
ulzirok <ulzirok@gmail.com>
2026-02-08 01:44:21 +0500

    Add a README

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
-// app.use(cors({
-//     origin: [
-//         'http://localhost:5173',
-//         'https://frontend'
-//     ]
-// }));
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://enchanting-centaur-aeb498.netlify.app'
+    ]
+}));
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
