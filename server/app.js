f743d55141a29205a54acb12077cd1283df65607
ulzirok <ulzirok@gmail.com>
2026-02-08 01:55:42 +0500

    Move cors settings to app.js

--- server/app.js
+++ server/app.js
@@ -3,7 +3,13 @@ const cors = require('cors');
 const songRoutes = require('./routes/songs.routes');
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://enchanting-centaur-aeb498.netlify.app'
+    ]
+}));
 
 app.use(express.json());
 app.use('/api/songs', songRoutes);
